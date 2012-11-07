2ab6f0aa4dd7171a4b4f4c5df076021f48c7e8e0
Jason <jason.addie@gmail.com>
2012-11-07 10:57:28 +0400

    fixed name of rails app

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-IRITrainingEvals::Application.configure do
+LinodeTest::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
